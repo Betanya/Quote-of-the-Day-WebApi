30396ad086ef42e65c8c133484c730e23b2e2a91
Jackson Siro <smataweb@gmail.com>
2017-03-29 16:17:06 +0300

    Create as_ajax_upload.php

---
+++ as_themes/admin/as_ajax_upload.php
@@ -0,0 +1,3 @@
+<script src="./as_themes/custom/jquery.min.js" type="text/javascript"></script>
+<script src="./as_themes/custom/jquery.uploadify.min.js" type="text/javascript"></script>
+<link rel="stylesheet" type="text/css" href="./as_themes/custom/uploadify.css">
